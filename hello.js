f740aa4121c9cff7ede9c99d8c43c66a185fd4a6
vijay <vijay@22feettribalww.com>
2018-02-05 16:13:30 +0530

    updated

--- hello.js
+++ hello.js
@@ -3,4 +3,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(8080, '0.0.0.0');
-console.log('Seddrver running at http://0.0.0.0:8080/');
+console.log('newrver running at http://0.0.0.0:8080/');
