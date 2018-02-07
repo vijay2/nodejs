1f21fb496d8cc219691a46a940761afc3ef781ea
vijay <vijay@22feettribalww.com>
2018-02-07 12:09:33 +0530

    updated 2-6

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8012, '0.0.0.0');
-console.log('Server running at http://139.59.74.228:8080/');
+}).listen(80, '0.0.0.0');
+console.log('Server running at http://139.59.74.228:80/');
